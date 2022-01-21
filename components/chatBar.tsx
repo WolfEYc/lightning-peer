95f1e7d400239b9dc8ab7ca20497f146a9df8a96
jadecss <ttjit963@gmail.com>
2022-01-21 16:45:39 +0000

    amaze

--- components/chatBar.tsx
+++ components/chatBar.tsx
@@ -1,11 +1,10 @@
+import {BiHash} from "react-icons/bi"
 
 const chatBar = () => {
     return(
-        <div className="fixed top-14 right-0 h-screen bg-gray-800 pt-6 pl-4 font-bold text-gray-500 text-sm w-1/5">
-            Message here
-            <input className = "align-text-top vertical-align:text-top bottom-6 border-8 flex-grow flex-col border-gray-700 right-8 rounded-lg m-0 fixed flex-1 bg-gray-700 h-11 w-1/6 focus:outline-none focus:text-white placeholder-gray-600" placeholder="Type message here...">
-            
-            </input>
+        <div className="fixed top-14 right-0 h-screen bg-gray-800 pt-6 pl-4 font-semibold text-white text-md w-1/5 flex">
+            <div className = "bottom-6 border-8 border-gray-700 right-4 rounded-lg m-0 fixed bg-gray-700 w-1/6 focus:outline-none focus:text-white font-normal placeholder-gray-600 textareaElement" contentEditable placeholder="Joe">
+            </div>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import HomeLogin from '../components/homeLogin'
 
 const Home: NextPage = () => {
   return (
-    <div className = "text-3xl h-screen bg-red text-white">
+    <><div className="h-12 min-w-full bg-pink-900"></div><div className="text-3xl h-screen bg-gray-800 text-white pt-2 px-96">
       <Head>
         <title>Lightning</title>
       </Head>
-      Now entering the twilight zone
-    </div>
+      <HomeLogin/>
+    </div></>
   )
 }
 
